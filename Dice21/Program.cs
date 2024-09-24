1dfbc413c2cf1efc65d71faf1eb9fad1aa679dc3
electricrbx <sixten.apelgren@gmail.com>
2024-09-24 13:33:40 +0200

    La till 21

---
+++ Dice21/Program.cs
@@ -0,0 +1,67 @@
+﻿namespace Dice21
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Welcome to 21");
+            Console.WriteLine("Do you wanna Hit or Stop");
+
+
+            string anwser = Console.ReadLine();
+            Random rand = new Random();
+            Random rand2 = new Random();
+            int dealer = 0;
+            int player = 0;
+      
+            while (anwser == "hit")
+            {
+                if (player <= 21 )
+                {
+                    if (dealer <= 17)
+                    {
+                        int randomNumber2 = rand2.Next(1, 7);
+                        dealer += randomNumber2;
+                        int DealerTotal = dealer;
+                        Console.WriteLine("Dealern slog " + randomNumber2);
+                        Console.WriteLine("Dealern har " + DealerTotal);
+
+                       
+                    }
+
+                    int randomNumber = rand.Next(1, 7);
+                    player += randomNumber;
+                    int playerTotal = player;
+                    Console.WriteLine("du slog " + randomNumber);
+                    Console.WriteLine("Du har " + playerTotal);
+                    Console.WriteLine("Do you wanna Hit or Stop");
+                    anwser = Console.ReadLine();
+
+
+
+                    while (anwser == "stop" && dealer <= 17)
+                    {
+                        int randomNumber3 = rand.Next(1, 7);
+                        dealer += randomNumber3;
+                        int DealerTotal2 = dealer;
+                        Console.WriteLine("Dealern slog " + randomNumber3);
+                        Console.WriteLine("Dealern har " + DealerTotal2);
+                    }
+
+
+                }
+                while (dealer <= player && player <= 22)
+                {
+                    Console.WriteLine("you won");
+                    break;
+                }
+                while  (player <= dealer && player >= 21)
+                {
+                    Console.WriteLine("you lost");
+                    break;
+                }
+            }
+        }
+    }
+}
